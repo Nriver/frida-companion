54de8ceb545c8fa14b40eefc3900e9aeade24979
Nriver <6752679+Nriver@users.noreply.github.com>
2021-11-12 17:30:42 +0800

    add load class event

--- bp/android/events.py
+++ bp/android/events.py
@@ -1,9 +1,25 @@
 import logging
 from __main__ import socketio
 
+from flask import render_template
+
 logger = logging.getLogger(__name__)
 
 
 @socketio.on('android_page', namespace='/')
 def android_page():
     print('android page')
+
+
+@socketio.on('load_classes', namespace='/')
+def refresh_device(message):
+    print('refresh_device()', message)
+
+    class_list = []
+
+    class_list_html = render_template("components/class_list.htm", **locals())
+
+    socketio.emit('load_class_response',
+                  {'data': {'class_list_html': class_list_html}})
+    
+    return class_list
